b7172ce6eda0fe8bf8d2f4d3a8eca71680b50c21
Ana Clara <48128230+anaclaravalentim@users.noreply.github.com>
2023-07-27 09:45:38 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5s!8+3($cui)@6l_dy*^olxwz%ga$!&xryi*&^3xw=tir=pp_^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
